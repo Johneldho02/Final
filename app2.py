0eb34be03466251e81f36b8d69677af1acc141a5
Johneldho02 <116822885+Johneldho02@users.noreply.github.com>
2024-04-28 22:39:08 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,40 @@
+from flask import Flask,request,render_template
+import pickle
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+
+
+data=pd.read_csv(r"C:\Users\johne\OneDrive\Desktop\ICTAK data\Social_Network_Ads.csv")
+
+with open('model2.pkl','rb') as file:
+    model=pickle.load(file)
+
+@app.route('/')   
+def home():
+    return render_template('index2.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    gen = str(request.form['gen'])
+    age = int(request.form['age'])
+    est_sal = int(request.form['est_sal'])
+
+    if gen=='Male':
+        genn=1
+    else:
+        genn=0   
+    feature=np.array([[genn,age,est_sal]])
+    ans=np.array(['No','Yes'])
+    prediction = model.predict(feature)
+    final=ans[prediction[0]]
+        
+    return render_template('index2.html',pred_res=final) 
+   
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+
+
+
